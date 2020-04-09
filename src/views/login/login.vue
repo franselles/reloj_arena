095a9b1ad28e62ffcf95bcb15ff0cfba600ea053
franselles <fran.selles@gmail.com>
2020-04-09 22:47:37 +0200

    Bug package

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -23,7 +23,7 @@
                 >
                   Acceder
                 </button>
-                <a href="#" class="float-right">0.1.31</a>
+                <a href="#" class="float-right">0.1.32</a>
               </form>
             </div>
           </div>
